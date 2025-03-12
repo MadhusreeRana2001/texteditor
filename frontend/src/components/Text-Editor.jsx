8868d350fe93f4446d22ad1b1e6ce8dd3e14a220
MadhusreeRana2001 <madhusree.rana2017@gmail.com>
2025-03-12 22:57:53 +0530

    Disabled ESLint in Text-Editor.js file

--- frontend/src/components/Text-Editor.jsx
+++ frontend/src/components/Text-Editor.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps, no-unused-vars */
+
 import { useEffect, useState, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import { saveAs } from "file-saver";
