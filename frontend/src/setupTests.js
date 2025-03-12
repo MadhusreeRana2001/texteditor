fcbbeb69d6ae0f25336896bfadaeae194158cc8b
MadhusreeRana2001 <madhusree.rana2017@gmail.com>
2025-03-12 22:26:18 +0530

    Added a new line to setupTests.js

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,4 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+process.env.ESLINT_NO_DEV_ERRORS = "true";
