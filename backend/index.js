29aabb8b19da1fb0f935d5f88d5b03056bdca77a
MadhusreeRana2001 <madhusree.rana2017@gmail.com>
2025-03-13 19:40:33 +0530

    Modified index.js file in the backend

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({ 
-    origin: [process.env.FRONTEND_URL, "http://192.168.144.84:3000"],
+    origin: [process.env.FRONTEND_URL],
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
     credentials: true }));
 
