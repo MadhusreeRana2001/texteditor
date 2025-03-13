32de07abb06b6edcd648d466407bdffe21ea5621
MadhusreeRana2001 <madhusree.rana2017@gmail.com>
2025-03-14 03:04:19 +0530

    Modified Google OAuth Redirect URI

--- backend/passportConfig.js
+++ backend/passportConfig.js
@@ -8,7 +8,7 @@ dotenv.config();
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback",
+  callbackURL: process.env.GOOGLE_REDIRECT_URI,
   passReqToCallback: true
 }, async (req, accessToken, refreshToken, profile, done) => {
   try {
